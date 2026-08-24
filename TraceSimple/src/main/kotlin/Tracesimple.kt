1cd82cc3cb69a3379b93ccc93adf00c087e72089
Mateo Lugo <2083785@cegepmontpetit.ca>
2026-08-24 10:03:56 -0400

    FCT TraceSimple

---
+++ TraceSimple/src/main/kotlin/Tracesimple.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val monNombre = .1 + .1 + .1
+    var maChaine = "Bonjour"
+    maChaine += " le monde"
+    println("monNombre = $monNombre")
+    println("maChaine = $maChaine")
+}
