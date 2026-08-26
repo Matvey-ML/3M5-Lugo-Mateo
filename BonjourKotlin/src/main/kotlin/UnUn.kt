20acf551246e4c6d522393b1049d2d9055da4789
Mateo Lugo Echeverri <2083785@cegepmontpetit.ca>
2026-08-26 16:33:25 -0400

    FCT Defi

--- BonjourKotlin/src/main/kotlin/UnUn.kt
+++ BonjourKotlin/src/main/kotlin/UnUn.kt
@@ -1,2 +1,28 @@
 package org.lugo
 
+fun main(){
+
+    println("Entrez un chaine de nombres")
+    val nombres = readln()
+
+    println(unUn(nombres))
+
+
+}
+fun unUn(source: String): String{
+    var result = ""
+    var count = 0
+    var currentChar = source[0]
+
+    for(i in source.indices){
+        if(source[i] == currentChar){
+            count++
+        }
+        else{
+            result += count.toString() + currentChar
+            currentChar = source[i]
+            count = 1
+        }
+    }
+    return result
+}
